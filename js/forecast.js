aee879c8c9cdc95b1d87b3dcf5078821d5315a6c
monicanq <monicanq@gmail.com>
2019-12-05 12:54:02 +0100

    modified api

--- js/forecast.js
+++ js/forecast.js
@@ -1,7 +1,6 @@
-
 class Forecast{
     constructor(){
-        this.key = APIKey;
+        this.key = 'Zdgkz1remcUCQrmfm3Qn4aJi3r9qyl21';
         this.weatherURI = 'https://dataservice.accuweather.com/currentconditions/v1/';
         this.cityURI = 'https://dataservice.accuweather.com/locations/v1/cities/search'
     }
